22d86d28f63542686bb16a61c2e97d8204010efc
DeepikaNarayanaswamy <deepiviji@gmail.com>
2016-08-15 15:59:15 +0530

    Basic Line chart done.

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivity.java
@@ -14,14 +14,19 @@ import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.db.chart.Tools;
-import com.db.chart.model.LineSet;
-import com.db.chart.view.AxisController;
-import com.db.chart.view.LineChartView;
-import com.db.chart.view.animation.Animation;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+import java.util.ArrayList;
 
 public class StockDetailActivity extends AppCompatActivity{
 
@@ -31,21 +36,21 @@ public class StockDetailActivity extends AppCompatActivity{
     private static final int STOCKS_LOADER = 1;
 
     private String currency;
-    private LineChartView lineChartView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_line_graph);
+        setContentView(R.layout.activity_stock_detail);
 
         //currency = getIntent().getStringExtra(TAG_STOCK_SYMBOL);
 
         //setTitle(currency);
-        lineChartView = (LineChartView) findViewById(R.id.linechart);
+
 
         //getLoaderManager().initLoader(STOCKS_LOADER, null, this);
-        String [] labels = {"a","b","c","d","e"};
+        /*String [] labels = {"a","b","c","d","e"};
         float [] values = new float[5];
         values[0] =16.001f;
         values[1] =15.031f;
@@ -63,8 +68,103 @@ public class StockDetailActivity extends AppCompatActivity{
                 .setYLabels(AxisController.LabelPosition.OUTSIDE)
                 .setXLabels(AxisController.LabelPosition.NONE)
                 .setLabelsColor(Color.parseColor("#6a84c3"))
-                .addData(dataset);
+                .addData(dataset);*/
 
+        /*ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.linechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+        series.addPoint(new ValueLinePoint("Jan", 2.4f));
+        series.addPoint(new ValueLinePoint("Feb", 3.4f));
+        series.addPoint(new ValueLinePoint("Mar", .4f));
+        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+        series.addPoint(new ValueLinePoint("Mai", 2.6f));
+        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+        series.addPoint(new ValueLinePoint("Nov", .4f));
+        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
+*/
+        /*LineChart chart = (LineChart) findViewById(R.id.linechart);
+
+        LineData data = new LineData();
+
+        data.addDataSet(getDataSet());
+        chart.setDescription("Stock price over time");
+        chart.setData(data);
+        setXAxis(chart);
+
+        chart.getAxisRight().setDrawLabels(false);
+        //chart.setAutoScaleMinMaxEnabled(true);
+        chart.setGridBackgroundColor(128);
+        chart.setBorderColor(255);
+        chart.setDrawGridBackground(false);
+        //chart.setBackgroundColor(0);
+        chart.getLegend().setEnabled(false);
+        chart.setPinchZoom(true);
+        chart.setDescription("");
+        chart.setTouchEnabled(true);
+        chart.setDoubleTapToZoomEnabled(true);
+        chart.animateXY(2000, 2000);
+        chart.invalidate();*/
+
+
+    }
+    private ILineDataSet getDataSet() {
+        ArrayList<ILineDataSet> dataSets = null;
+        ArrayList<Entry> valueSet1 = new ArrayList<>();
+
+        Entry v1e1 = new Entry(10.000f, 0);
+        valueSet1.add(v1e1);
+        Entry v1e2 = new Entry(20.000f, 4);
+        valueSet1.add(v1e2);
+        Entry v1e3 = new Entry(30.000f, 5);
+        valueSet1.add(v1e3);
+        Entry v1e4 = new Entry(40.000f, 6);
+        valueSet1.add(v1e4);
+        Entry v1e5 = new Entry(50.000f, 7);
+        valueSet1.add(v1e5);
+        Entry v1e6 = new Entry(60.000f, 8);
+        valueSet1.add(v1e6);
+
+        ILineDataSet lineDataSet1 = new LineDataSet(valueSet1, "Brand 1");
+        int[] colors  = new int[] {Color.RED  ,Color.RED, Color.BLUE ,Color.GREEN, Color.GRAY, Color.RED};
+
+
+        return lineDataSet1;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        xAxis.add("JUL");
+        xAxis.add("AUG");
+        xAxis.add("SEPT");
+        xAxis.add("OCT");
+        xAxis.add("NOV");
+        xAxis.add("DEC");
+        return xAxis;
+    }
+
+    private void setXAxis(LineChart chart){
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setDrawLabels(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.RED);
+        //xAxis.setDrawGridLines(true);
+        xAxis.setDrawAxisLine(true);
     }
 
 }
--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivityFragment.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivityFragment.java
@@ -2,13 +2,36 @@ package com.sam_chordas.android.stockhawk.ui;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.sam_chordas.android.stockhawk.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,7 +40,7 @@ public class StockDetailActivityFragment extends Fragment {
 
     public StockDetailActivityFragment() {
     }
-
+    LineChart chart ;        JSONObject results = null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,6 +50,198 @@ public class StockDetailActivityFragment extends Fragment {
         View stockDetailView = inflater.inflate(R.layout.fragment_stock_detail, container, false);
         TextView stockName =  (TextView)stockDetailView.findViewById(R.id.stockName);
         stockName.setText(stockSymbol);
+        chart = (LineChart) stockDetailView.findViewById(R.id.linechart);
+        FetchHistoricalDataTask asyncTask = new FetchHistoricalDataTask();
+        asyncTask.execute(stockSymbol);
         return stockDetailView;
+    }
+
+
+    private class FetchHistoricalDataTask extends AsyncTask<String,Void,Void>
+    {
+        String url="https://query.yahooapis.com/v1/public/yql";
+        String Search="format";
+        String SearchVal="json";
+        String QueryKey="q";
+        String Diag ="diagnostics";
+        String DiagVal="true";
+        String Env="env";
+        String EnvVal="store://datatables.org/alltableswithkeys";
+        String Call="callback";
+        String CallVal="";
+
+        @Override
+        protected Void doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+            String stockName = params[0];
+            String query="Select * from yahoo.finance.historicaldata where symbol ='"
+                    + stockName + "' and startDate = '2016-01-01' and endDate = '2016-01-25'";
+
+
+            Log.v("OUTPUT", "ENTERING doINbackground method");
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                Uri buildUri;
+
+                buildUri=Uri.parse(url).buildUpon()
+                        .appendQueryParameter(QueryKey,query)
+                        .appendQueryParameter(Search,SearchVal)
+                        .appendQueryParameter(Diag,DiagVal)
+                        .appendQueryParameter(Env,EnvVal)
+                        .appendQueryParameter(Call,CallVal)
+                        .build();
+                Log.v("OUTPUT",buildUri.toString());
+                // Create the request to OpenWeatherMap, and open the connection
+                URL url = new URL(buildUri.toString());
+                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                Log.v("OUTPUT","CONNECTED !!!");
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                Log.v("OUTPUT","READING");
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                results = new JSONObject(forecastJsonStr);
+
+
+
+            } catch (JSONException ex){
+                ex.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+           LineData data = new LineData();
+            data.addDataSet(getDataSet());
+            chart.setDescription("Stock price over time");
+            chart.setData(data);
+            setXAxis(chart);
+
+            chart.getAxisRight().setDrawLabels(false);
+            //chart.setAutoScaleMinMaxEnabled(true);
+            chart.setGridBackgroundColor(128);
+            chart.setBorderColor(255);
+            chart.setDrawGridBackground(false);
+            //chart.setBackgroundColor(0);
+            chart.getLegend().setEnabled(false);
+            chart.setPinchZoom(true);
+            chart.setDescription("");
+            chart.setTouchEnabled(true);
+            chart.setDoubleTapToZoomEnabled(true);
+            chart.animateXY(2000, 2000);
+            chart.invalidate();
+        }
+    }
+    private ILineDataSet getDataSet() {
+        JSONArray quoteArray = null;
+        ILineDataSet lineDataSet1 = null;
+        try {
+            JSONObject query1 = (JSONObject) results.get("query");
+            JSONObject RESULTS = query1.getJSONObject("results");
+            quoteArray = RESULTS.getJSONArray("quote");
+
+
+        ArrayList<ILineDataSet> dataSets = null;
+        ArrayList<Entry> valueSet1 = new ArrayList<>();
+        for (int i=0;i<quoteArray.length();i++){
+
+            JSONObject quoteValue = quoteArray.getJSONObject(i);
+            String date = null;
+             date = (String)quoteValue.get("Date");
+            String value = (String)quoteValue.get("Adj_Close");
+            SimpleDateFormat formattedDate = new SimpleDateFormat(date);
+            Entry v1e1 = new Entry(i,Float.parseFloat(value));
+                    valueSet1.add(v1e1);
+        }
+
+
+          lineDataSet1 = new LineDataSet(valueSet1, "Brand 1");
+        int[] colors  = new int[] {Color.RED  ,Color.RED, Color.BLUE ,Color.GREEN, Color.GRAY, Color.RED};
+
+        }catch(JSONException ex){
+            ex.printStackTrace();
+        }
+
+        return lineDataSet1;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        xAxis.add("JUL");
+        xAxis.add("AUG");
+        xAxis.add("SEPT");
+        xAxis.add("OCT");
+        xAxis.add("NOV");
+        xAxis.add("DEC");
+        return xAxis;
+    }
+
+    private void setXAxis(LineChart chart){
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setDrawLabels(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.RED);
+        //xAxis.setDrawGridLines(true);
+        xAxis.setDrawAxisLine(true);
     }
 }
